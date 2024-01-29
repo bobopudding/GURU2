ce5cebf1b2ab92b4b4857d40940e8d1bef63c14e
woodyej <126846420+woodyej@users.noreply.github.com>
2024-01-29 11:26:47 +0900

    주간예보, 마이페이지, 패션 스타일 변경, 스타일 더보기 xml

--- app/src/main/java/com/example/guru2/WeathercastActivity.kt
+++ app/src/main/java/com/example/guru2/WeathercastActivity.kt
@@ -2,10 +2,32 @@ package com.example.guru2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 
 class WeathercastActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weathercast)
+
+        //툴바 적용
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        val ab = supportActionBar!!
+        ab.setDisplayShowTitleEnabled(false)
+        ab.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    //툴바 뒤로가기 버튼 동작
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        when (id) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
